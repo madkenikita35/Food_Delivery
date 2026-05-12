1c8573e6d26673e14765c1fc4fbb59236682c4d5
Nikita Satish Madke <madkenikita35@gmail.com>
2026-05-12 23:30:41 +0530

    Add

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const NavBar = () => {
   return (
-    <nav className="bg-[#2D1B0E] border-b-8 border-b-8 border-amber-900/30 shadow-amber-900/30 sticky top-0 z-50 shadow-[0_25px_50px_12px] font-vibes group/nav overflow-hidden">
+    <nav className="bg-[#2D1B0E] border-b-8 border-b-8 border-amber-900/30 shadow-amber-900/30 sticky top-0 z-50 shadow-[0_25px_50px_12px] font-vibes group/nav overflow-hidden ">
       <div className=" absolute top-3 left-1/2 -translate-x-1/2 w-full max-w-7xl px-4">
         <div className="h-[6px]"></div>
       </div>
